567c64f86f472082b2ad8c506ff96b781963d464
[ignaciocaff] <[jigcaffaratti@gmail.com]>
2017-08-29 16:18:12 -0300

    test2

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,5 @@
 ﻿export class AppConfig {
-    public readonly apiUrl = 'http://localhost:59570/api/';
+    public readonly apiUrl = 'http://localhost:59570/api/2';
     //public readonly apiUrl = 'http://daniandreoli-001-site1.dtempurl.com/api/';
     // public readonly apiUrl = 'http://192.168.0.100/api/';
     // public readonly apiUrl = 'http://localhost/api/';
